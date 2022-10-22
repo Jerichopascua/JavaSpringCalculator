9a64da8b73fbb88a8808815ef4fdbbb8cb3d4d51
GitLito <jericho.pascua@gmail.com>
2022-10-22 22:32:27 +0800

    CALCULATOR1

--- src/main/java/com/example/calculator/basic/BasicController.java
+++ src/main/java/com/example/calculator/basic/BasicController.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,34 @@ public class BasicController {
          return getResultFromError("no division by null");
       }
       return getResult(dividend.divide(divisor, RoundingMode.HALF_EVEN));
+   }
+   
+
+   @GetMapping("/shipping")
+   public ApiResult shipping(@RequestParam double packageWeight, @RequestParam double packageHeight, @RequestParam double packageWidth,@RequestParam double packagLength) {
+      double finalPrice = 0;
+
+      double packagVolume = 0;
+      
+      //Computations
+
+      packagVolume=	packageHeight * packageWidth * packagLength;
+      if (packageWeight > 50) {
+    	  finalPrice = 0;
+      }else if (packageWeight >= 10)  {
+    	  finalPrice = 20 * packageWeight;
+      }else if (packagVolume <=1500)  {
+    	  finalPrice = 0.03 *packagVolume;
+      }else if (packagVolume >1500  &&  packagVolume <=2500)  {
+    	  finalPrice = 0.04 *packagVolume;	  
+      }else  {
+    	  finalPrice = 0.05 *packagVolume;	 
+
+      }
+      
+      
+      return getResult(finalPrice);
+      
    }
 
 }
